7755c7cf89634c01ff2acb8f612eb7902fbd5cce
leecharleslaing <lcharleslaing@protonmail.com>
2022-02-27 00:22:18 -0500

    Added blank target links to contact links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,26 +31,32 @@
         questions, comments, please feel free to reach out to me by <a
           class="text-blue-600"
           href="mailto:lcharleslaing@protonmail.com">sending me an email</a
-        >.
+        >. Also, if you need any webdevelpment, feel free to reach out to me and
+        we can discuss your needs.
       </p>
+      <p class="p-6">Have an Amazing Day and Let's make America Great Again!</p>
     </div>
     <a
+      target="_blank"
       href="https://web.telegram.org/z/#-1503039356"
       class="m-3 p-3 text-xl text-slate-100 shadow-md bg-accent rounded-3xl"
       >@Big2Tiny Truth</a
     >
     <a
+      target="_blank"
       href="https://web.telegram.org/z/#-1503039356"
       class="m-3 p-3 text-xl text-slate-100 shadow-md bg-accent rounded-3xl"
       >@big2tiny-Lee</a
     >
     <a
+      target="_blank"
       href="https://signal.group/#CjQKIBdo5RraCIxDoFnWMynrMQJ4R2n60cgT9gmQwcdmmnNUEhAnYhoDMV_s0vs2XZMJFfSD"
       class="m-3 p-3 text-xl text-slate-100 shadow-md bg-secondary rounded-3xl"
       >Signal Big2TinyTruth Group</a
     >
 
     <a
+      target="_blank"
       href="mailto:lcharleslaing@protonmail.com"
       class="m-3 p-3 text-xl text-slate-900 shadow-md bg-primary rounded-3xl"
       >Email Me</a
