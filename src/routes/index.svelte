0d4b616732a48dc179afac43bfc15bf8984888ea
leecharleslaing <lcharleslaing@protonmail.com>
2022-02-26 23:12:41 -0500

    Added home page buttons

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<div class="container flex flex-wrap justify-evenly gap-6">
+  <div class="card rounded-2xl bg-primary p-6 text-4xl">
+    <a target="_blank" href="https://qagg.news/">Qaag.news</a>
+  </div>
+  <div class="card rounded-2xl bg-primary p-6 text-4xl">
+    <a href="/gematria_decodes">MY Gematria</a>
+  </div>
+  <div class="card rounded-2xl bg-primary p-6 text-4xl">
+    <a target="_blank" href="http://truthsocial.com/">Truth Social</a>
+  </div>
+  <div class="card rounded-2xl bg-primary p-6 text-4xl">
+    <a target="_blank" href="https://signal.org/">Signal.org</a>
+  </div>
+  <div class="card rounded-2xl bg-primary p-6 text-4xl">
+    <a target="_blank" href="https://www.gematrix.org/">Gematrix.org</a>
+  </div>
+</div>
